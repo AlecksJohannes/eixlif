eb5bd072a82ee2885e9d7b79cb767f7fa00f62c9
AlecksJohannes <alecksjohanssen@gmail.com>
2017-11-25 10:50:08 +0700

    get movies by request and add loading screen

--- src/adapter/MoviesList.jsx
+++ src/adapter/MoviesList.jsx
@@ -1,22 +1,39 @@
 import React, { Component } from 'react';
 import {getMoviesFromApi} from '../http/Request';
+import Movie from '../model/Movie';
 
 class MoviesList extends Component {
 
-	componentDidMount() {
-		getMoviesFromApi().then((response) => {
-			this.setState({
+  constructor(props) {
+    super(props);
+    this.state = {
+      movies: [],
+      isLoading: true
+    }
+  }
 
-			})
-		});
-	}
+  componentDidMount() {
+    setTimeout(() => {
+        getMoviesFromApi().then((response) => {
+        this.setState({
+          movies: response,
+          isLoading: false
+        })
+      })
+    }, 4000)
+  }
 
   render() {
-    return(
-      <div>
-        Hello
-      </div>
-    )
+    let content;
+    if(this.state.isLoading) {
+      return (<h1> Loading </h1>)
+    } else {
+      return(
+        this.state.movies.map((movie) =>
+          <Movie {...movie}/>
+        )
+      )
+    }
   }
 }
 
--- src/http/Request.jsx
+++ src/http/Request.jsx
@@ -5,12 +5,12 @@ const url = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5
 export { getMoviesFromApi };
 
 function getMoviesFromApi() {
-	return fetch(url)
-		.then((response) => response.json())
-		.then((responseJson) => {
-			return responseJson.results;
-		})
-		.catch((error) => {
-			console.error(error);
-		});
+  return fetch(url)
+    .then((response) => response.json())
+    .then((responseJson) => {
+      return responseJson.results;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
 }
